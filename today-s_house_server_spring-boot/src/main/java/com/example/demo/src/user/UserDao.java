a447c741129cd7da166f5314489e25ae7dfbd85c
benjamin <chosj1526@gmail.com>
2022-09-20 19:37:31 +0900

    feat : ADD SIGN-UP (TODAY-S_HOUSE API SERVER SPRING BOOT)

---
+++ today-s_house_server_spring-boot/src/main/java/com/example/demo/src/user/UserDao.java
@@ -0,0 +1,112 @@
+package com.example.demo.src.user;
+
+
+import com.example.demo.src.user.model.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.util.List;
+
+@Repository
+public class UserDao {
+
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public void setDataSource(DataSource dataSource){
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    public List<GetUserRes> getUsers(){
+        String getUsersQuery = "select * from UserInfo";
+        return this.jdbcTemplate.query(getUsersQuery,
+                (rs,rowNum) -> new GetUserRes(
+                        rs.getInt("userIdx"),
+                        rs.getString("userName"),
+                        rs.getString("ID"),
+                        rs.getString("Email"),
+                        rs.getString("password"))
+                );
+    }
+
+    public List<GetUserRes> getUsersByEmail(String email){
+        String getUsersByEmailQuery = "select * from UserInfo where email =?";
+        String getUsersByEmailParams = email;
+        return this.jdbcTemplate.query(getUsersByEmailQuery,
+                (rs, rowNum) -> new GetUserRes(
+                        rs.getInt("userIdx"),
+                        rs.getString("userName"),
+                        rs.getString("ID"),
+                        rs.getString("Email"),
+                        rs.getString("password")),
+                getUsersByEmailParams);
+    }
+
+    public GetUserRes getUser(int userIdx){
+        String getUserQuery = "select * from UserInfo where userIdx = ?";
+        int getUserParams = userIdx;
+        return this.jdbcTemplate.queryForObject(getUserQuery,
+                (rs, rowNum) -> new GetUserRes(
+                        rs.getInt("userIdx"),
+                        rs.getString("userName"),
+                        rs.getString("ID"),
+                        rs.getString("Email"),
+                        rs.getString("password")),
+                getUserParams);
+    }
+    
+
+    public int createUser(PostUserReq postUserReq){
+        String createUserQuery = "insert into USER (EMAIL, PASSWORD, NICKNAME) VALUES (?,?,?)";
+        Object[] createUserParams = new Object[]{postUserReq.getEMAIL(), postUserReq.getPASSWORD(), postUserReq.getNICKNAME()};
+        this.jdbcTemplate.update(createUserQuery, createUserParams);
+
+        String lastInserIdQuery = "select last_insert_id()";
+        return this.jdbcTemplate.queryForObject(lastInserIdQuery,int.class);
+        
+    }
+
+    public int checkEmail(String email){
+        String checkEmailQuery = "select exists(select EMAIL from USER where EMAIL = ?)";
+        return this.jdbcTemplate.queryForObject(checkEmailQuery,
+                int.class,
+                email);
+
+    }
+
+    public int checkNickname(String nickname){
+        String checkNicknameQuery = "select exists(select NICKNAME from USER where NICKNAME = ?)";
+        return this.jdbcTemplate.queryForObject(checkNicknameQuery,
+                int.class,
+                nickname);
+
+    }
+
+    public int modifyUserName(PatchUserReq patchUserReq){
+        String modifyUserNameQuery = "update UserInfo set userName = ? where userIdx = ? ";
+        Object[] modifyUserNameParams = new Object[]{patchUserReq.getUserName(), patchUserReq.getUserIdx()};
+
+        return this.jdbcTemplate.update(modifyUserNameQuery,modifyUserNameParams);
+    }
+
+    public User getPwd(PostLoginReq postLoginReq){
+        String getPwdQuery = "select userIdx, password,email,userName,ID from UserInfo where ID = ?";
+        String getPwdParams = postLoginReq.getId();
+
+        return this.jdbcTemplate.queryForObject(getPwdQuery,
+                (rs,rowNum)-> new User(
+                        rs.getInt("userIdx"),
+                        rs.getString("ID"),
+                        rs.getString("userName"),
+                        rs.getString("password"),
+                        rs.getString("email")
+                ),
+                getPwdParams
+                );
+
+    }
+
+
+}
